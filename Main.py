aeb082c7517b1e8f32b736fbfe1a5ad63ca4efb1
LIZZI Léo <32718789+leolizzi@users.noreply.github.com>
2018-05-05 22:49:25 +0200

    Add files via upload
    Création d'un main qui fonctionne et modifications annexes notamment gestion des "erreurs d'authentification".

--- Main.py
+++ Main.py
@@ -8,27 +8,8 @@ import os
 import signal
 from fonctions import *
 
-#Lis les tags en continu
+#On efface la console
+os.system('clear')
+
+#On lit les cartes ou tags en continue
 read()
-
-#Verifie l'authentification
-if status == MIFAREReader.MI_OK:
-    codeLu = MIFAREReader.MFRC522_Read(8)
-    MIFAREReader.MFRC522_StopCrypto1()
-
-    #On converti le code en string
-    codeCarte = lstToString(codeLu)
-
-    #On cherche le code dans la base de donnnee
-    print rechercheIdentifiant(codeCarte)
-    time.sleep(2)
-    os.system('clear')
-
-else:
-    print 'ERREUR D\'AUTHENTIFICATION'+'\n'
-    time.sleep(2)
-    os.system('clear')
-
-
-
-            
--- fonctions.py
+++ fonctions.py
@@ -9,6 +9,76 @@ import os
 import signal
 import RPi.GPIO as GPIO
 
+#GPIO.setwarning(False)
+#GPIO.setmode(GPIO.BCM)
+
+def read():
+
+    ''' Lit les code en continue '''
+
+    global status
+    global autorisation
+    global sortie
+
+    continue_reading = True
+
+    # Capture SIGINT for cleanup when the script is aborted
+    def end_read(signal,frame):
+        global continue_reading
+        print 'FIN DE LECTURE'
+        continue_reading = False
+        GPIO.cleanup()
+
+    # Hook the SIGINT
+    signal.signal(signal.SIGINT, end_read)
+
+    # Create an object of the class MFRC522
+    MIFAREReader = MFRC522.MFRC522()
+
+    #print "CTRL+C POUR STOPPER LA LECTURE"
+
+    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+    while continue_reading:
+
+        # Scan for cards
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+        # Get the UID of the card
+        (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == MIFAREReader.MI_OK:
+
+            # This is the default key for authentication
+            key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+
+            # Select the scanned tag
+            MIFAREReader.MFRC522_SelectTag(uid)
+
+            # Authenticate
+            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+            # Check if authenticated
+            if status == MIFAREReader.MI_OK:
+                codeLu = MIFAREReader.MFRC522_Read(8)
+                MIFAREReader.MFRC522_StopCrypto1()
+
+                #On converti le code en string
+                codeCarte = lstToString(codeLu)
+
+                #On cherche le code dans la base de donnnee
+                rechercheIdentifiant(codeCarte)
+                time.sleep(2)
+                os.system('clear')
+
+            else:
+                autorisation = False
+                print 'ERREUR D\'AUTHENTIFICATION'
+                print 'ACCES REFUSE'
+                redBlink(8)
+                time.sleep(2)
+                os.system('clear')
+                
 def lstToString(codeLecteur):
 
     ''' Convertis une liste en string '''
@@ -20,49 +90,6 @@ def lstToString(codeLecteur):
 
     return codeCarte
 
-def read():
-
-    ''' Cherche des tags RFiD a lire en continu '''
-    
-    continue_reading = True
-
-    #On efface la console
-    os.system('clear')
-
-    #Capture SIGINT for cleanup when the script is aborted
-    def end_read(signal,frame):
-        global continue_reading
-        print 'FIN DE LECTURE'
-        continue_reading = False
-        GPIO.cleanup()
-
-    #Hook the SIGINT
-    signal.signal(signal.SIGINT, end_read)
-
-    #Create an object of the class MFRC522
-    MIFAREReader = MFRC522.MFRC522()
-
-    #This loop keeps checking for chips. If one is near it will get the UID and authenticate
-    while continue_reading:
-
-        #Scan for cards
-        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
-        #Get the UID of the card
-        (status,uid) = MIFAREReader.MFRC522_Anticoll()
-
-        #If we have the UID, continue
-        if status == MIFAREReader.MI_OK:
-
-            #This is the default key for authentication
-            key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-
-            #Select the scanned tag
-            MIFAREReader.MFRC522_SelectTag(uid)
-
-            #Authenticate
-            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
 def rechercheIdentifiant(codeCarte):
 
     ''' Recherche un code dans une base de donnees sqlite '''
@@ -71,7 +98,7 @@ def rechercheIdentifiant(codeCarte):
     global cursor
     global sortie
     global autorisation
-
+    
     #On se connecte à la base
     conn = sqlite3.connect('datab.db')
     cursor = conn.cursor()
@@ -116,30 +143,31 @@ def rechercheIdentifiant(codeCarte):
 
           #La date
           ajourdHui = datetime.datetime.today()
-
+          
           #Le jour de la semaine
           jour = ajourdHui.weekday()
-
+          
           #On l'heure en minute
-          minute = date[4]+60*date[3]
+          minute = date[4]+60*date[3] 
 
-          #C'est un professeur
+          #C'est un professeur 
           if statut == 'PROFESSEUR':
 
 
                  #On n'est pas le weekend
                  if jour != 5 and jour != 6:
                      autorisation = True
-
+                                         
                  #On est le weekend
                  else:
                      autorisation = False
-
+    
           #C'est un agent
           elif statut == 'AGENT':
                  autorisation = True
 
           #C'est un eleve
+
           else:
 
                  #Il est entre 7H30 et 18H30
@@ -148,11 +176,11 @@ def rechercheIdentifiant(codeCarte):
                      #On n'est pas le weekend
                      if jour != 5 and jour != 6:
                          autorisation = True
-
+                         
                      #On est le weekend
                      else:
                          autorisation = False
-
+        
                  #Hors plage horaire
                  else:
                         autorisation = False
@@ -162,69 +190,40 @@ def rechercheIdentifiant(codeCarte):
 
           autorisation = False
 
-    if autorisation == True:
-        greenBlink(3)
+    if autorisation:
         sortie += '\n'+'ACCES AUTORISE'+'\n'
+        print sortie
+        greenBlink(3)
     else:
-        redBlink(8)
         sortie += '\n'+'ACCES REFUSE'+'\n'
-
-    return sortie
+        print sortie
+        redBlink(8)
 
 def greenBlink(greenPin):
-
-    ''' Fais clignoter la LED verte '''
-    
     GPIO.setup(greenPin,GPIO.OUT)
-
+    
     for i in range(2):
         GPIO.output(greenPin,GPIO.HIGH)
         time.sleep(.1)
         GPIO.output(greenPin,GPIO.LOW)
         time.sleep(.1)
-
+        
     GPIO.output(greenPin,GPIO.HIGH)
     time.sleep(.5)
     GPIO.output(greenPin,GPIO.LOW)
 
 def redBlink(redPin):
-
-    ''' Fais clignoter la LED rouge '''
-    
     GPIO.setup(redPin,GPIO.OUT)
-
+    
     for i in range(4):
         GPIO.output(redPin,GPIO.HIGH)
         time.sleep(.1)
         GPIO.output(redPin,GPIO.LOW)
         time.sleep(.1)
-
+        
     GPIO.output(redPin,GPIO.HIGH)
     time.sleep(.5)
     GPIO.output(redPin,GPIO.LOW)
 
-def buzzerTrue(buzzerPin):
-
-    ''' Fait buzzer le buzzer  quand le passage est autorise '''
-
-    GPIO.setup(buzzerPin,GPIO.OUT)
-
-    for i in range(4):
-        GPIO.output(buzzerPin,GPIO.HIGH)
-        time.sleep(.1)
-        GPIO.output(buzzerPin,GPIO.LOW)
-
-def buzzerWrong(buzzerPin):
-
-    ''' Fait buzzer le buzzer  quand le passage est non-autorise '''
-
-    GPIO.setup(buzzerPin,GPIO.OUT)
-
-    GPIO.output(buzzerPin,GPIO.HIGH)
-    time.sleep(2)
-    GPIO.output(buzzerPin,GPIO.LOW)
-
-
-    
 
     
